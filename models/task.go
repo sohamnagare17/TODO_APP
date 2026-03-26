a2287630363305c8173d9b2b2428d7decfd233d2
siddharthkumbhar-code <siddharth.kumbhar@cctech.co.in>
2026-03-26 12:18:17 +0530

    updated models

--- models/task.go
+++ models/task.go
@@ -1,8 +1,19 @@
- package models
+package models
+
+import "time"
  
 
 type Task struct{
+	Userid int `json:"userId"`
 	ID int  `json:"id"`
 	NAME string `json:"name"`
 	STATUS  string `json:"status"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type User struct{
+	Userid int `json:"userId"`
+	UserName string `json:"username"`
+	Email string `json:"email"`
 }
