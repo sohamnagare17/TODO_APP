32e93ef0a1e2fca3bd8c5cd63b8f4ca4e55d952f
siddharthkumbhar-code <siddharth.kumbhar@cctech.co.in>
2026-03-26 13:23:21 +0530

    Insert Task Updated

--- handlers/InsertTask.go
+++ handlers/InsertTask.go
@@ -1,27 +1,38 @@
 package handlers
 
- import (
-	"encoding/json"
-		"net/http"
-		"log"
+import (
 	"database/sql"
-		"go-sqlite/models"
-  )
+	"encoding/json"
+	"go-sqlite/models"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
 
 
 func InsertTask(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request){
         var newtask models.Task;
 
-		err := json.NewDecoder(r.Body).Decode(&newtask)
+		uid:=r.URL.Query().Get("userid")
+		userid,err:=strconv.Atoi(uid)
+		if err!=nil{
+			log.Println("enter valid user id")
+		}
+
+		err= json.NewDecoder(r.Body).Decode(&newtask)
 
 		if err!=nil{
 		log.Println("error in fetching the data")
 		}
 
-		query := `INSERT INTO tasks (name , status) VALUES(?,?)`
+		now:= time.Now().Format("2006-01-02 15:04:05")
 
-		_, err = db.Exec(query,newtask.NAME,newtask.STATUS)
+		query := `INSERT INTO tasks (name,status,createdAt,updatedAt,userId) 
+				VALUES(?,?,?,?,?)`
+
+		_, err = db.Exec(query,newtask.NAME,newtask.STATUS,now,now,userid)
 
 		if err != nil{
 		log.Println("somthing went wrong to inserting the data ")
