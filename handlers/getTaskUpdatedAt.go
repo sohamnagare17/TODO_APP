04f0c9643dd62a8e5e8bec744e8ab5c735c16ae0
siddharthkumbhar-code <siddharth.kumbhar@cctech.co.in>
2026-03-26 16:39:23 +0530

    getTasks file changes

---
+++ handlers/getTaskUpdatedAt.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"go-sqlite/models"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func GetTaskUpdatedAt(db *sql.DB) http.HandlerFunc{
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	id := r.URL.Query().Get("userid")
+	if id == "" {
+		log.Fatal("Id is missing")
+	}
+	iid, err := strconv.Atoi(id)
+	if err != nil {
+		log.Println("Enter a valid id")
+	}
+	query := `SELECT * FROM tasks 
+			WHERE userId = ?
+			ORDER BY updatedAt DESC`
+
+	rows, err := db.Query(query, iid)
+	if err != nil {
+		log.Fatal("Internal server Error")
+	}
+	var task models.Task
+	tasks := []models.Task{}
+
+	for rows.Next() {
+		rows.Scan(&task.ID, &task.NAME, &task.STATUS, &task.CreatedAt, &task.UpdatedAt, &task.Userid)
+		tasks = append(tasks, task)
+	}
+	rows.Close()
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+
+}
+
+}
