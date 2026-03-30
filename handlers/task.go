27b9a71e24970144459c88e6a59bfc7cdf78d474
siddharthkumbhar-code <siddharth.kumbhar@cctech.co.in>
2026-03-30 12:46:05 +0530

    change in GetTasksBySorted

--- handlers/task.go
+++ handlers/task.go
@@ -10,152 +10,150 @@ import (
 	"time"
 )
 
-func GetTaskByUserId(db *sql.DB) http.HandlerFunc{
-	return func( writer http.ResponseWriter ,request *http.Request){
+func GetTaskByUserId(db *sql.DB) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
 
-		var tasklist []models.Task;
-
+		var tasklist []models.Task
 
 		// userIDStr := r.PathValue("userId")
 
-        // userID, err := strconv.Atoi(userIDStr)
-        // if err != nil {
-        //     http.Error(w, "invalid userId", http.StatusBadRequest)
-        //     return
-        // }
-		
+		// userID, err := strconv.Atoi(userIDStr)
+		// if err != nil {
+		//     http.Error(w, "invalid userId", http.StatusBadRequest)
+		//     return
+		// }
+
 		useridstr := request.PathValue("userid")
 
-		if useridstr ==""{
+		if useridstr == "" {
 			log.Println("id required plz!")
 			return
 		}
 
-		userid,err := strconv.Atoi(useridstr);
-		if err!=nil{
+		userid, err := strconv.Atoi(useridstr)
+		if err != nil {
 			log.Println("id must be the number")
-			return 
+			return
 		}
 		query := `SELECT id,name,status,userid FROM tasks1 WHERE userid=?`
 
-		rows,err1:=db.Query(query,userid)
-		for rows.Next(){
+		rows, err1 := db.Query(query, userid)
+		for rows.Next() {
 			var task models.Task
 
-			err = rows.Scan(&task.ID,&task.NAME,&task.STATUS,&task.USERID)
-			if err1!=nil{
-				log.Println("error in scanning the data from the rows",err)
+			err = rows.Scan(&task.ID, &task.NAME, &task.STATUS, &task.USERID)
+			if err1 != nil {
+				log.Println("error in scanning the data from the rows", err)
 			}
 			tasklist = append(tasklist, task)
 		}
-		if err!=nil{
+		if err != nil {
 			log.Println("error in fetching the data")
-			return 
+			return
 		}
 
 		json.NewEncoder(writer).Encode(map[string]interface{}{
-			"message":"the task of the user are",
-			"tasklist":tasklist,
+			"message":  "the task of the user are",
+			"tasklist": tasklist,
 		})
 	}
 }
 
 func InsertTask(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request){
-        var newtask models.Task;
+	return func(w http.ResponseWriter, r *http.Request) {
+		var newtask models.Task
 
-		 userIDStr := r.PathValue("userId")
+		userIDStr := r.PathValue("userId")
 
-        userID, err := strconv.Atoi(userIDStr)
-        if err != nil {
-            http.Error(w, "invalid userId", http.StatusBadRequest)
-            return
-        }
+		userID, err := strconv.Atoi(userIDStr)
+		if err != nil {
+			http.Error(w, "invalid userId", http.StatusBadRequest)
+			return
+		}
 
 		err = json.NewDecoder(r.Body).Decode(&newtask)
 
-		if err!=nil{
-		log.Println("error in fetching the data")
+		if err != nil {
+			log.Println("error in fetching the data")
 		}
 
 		query := `INSERT INTO tasks1 (name ,status,userid,createdAt,updatedAt) VALUES(?,?,?,?,?)`
-		now:=time.Now().UTC().Format(time.RFC3339)
+		now := time.Now().UTC().Format(time.RFC3339)
 
-		_, err = db.Exec(query,newtask.NAME,newtask.STATUS,userID,now,now)
-		
+		_, err = db.Exec(query, newtask.NAME, newtask.STATUS, userID, now, now)
 
-		if err != nil{
-		log.Println("somthing went wrong to inserting the data ",err)
-		return 
+		if err != nil {
+			log.Println("somthing went wrong to inserting the data ", err)
+			return
 		}
-		
+
 		w.Header().Set("Content-type", "application/json")
 		json.NewEncoder(w).Encode(map[string]interface{}{
-		"message":"the task inserted succesfully into database ",
-		"taskname":newtask.NAME,
-		"userid":userID,
+			"message":  "the task inserted succesfully into database ",
+			"taskname": newtask.NAME,
+			"userid":   userID,
 		})
 
 	}
 }
 
-func GetTaskByStatus(db *sql.DB) http.HandlerFunc{
-	return func( writer http.ResponseWriter ,request *http.Request){
+func GetTaskByStatus(db *sql.DB) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
 
-		var tasklist []models.Task;
+		var tasklist []models.Task
 		useridstr := request.PathValue("userid")
 		status := request.PathValue("status")
 
-		if useridstr ==""{
+		if useridstr == "" {
 			log.Println("id required plz!")
 			return
 		}
 
-		userid,err := strconv.Atoi(useridstr);
-		if err!=nil{
+		userid, err := strconv.Atoi(useridstr)
+		if err != nil {
 			log.Println("id must be the number")
-			return 
+			return
 		}
 		query := `SELECT id,name,status,userid FROM tasks1 WHERE userid=? AND status=?`
 
-		rows,err1:=db.Query(query,userid,status)
-		for rows.Next(){
+		rows, err1 := db.Query(query, userid, status)
+		for rows.Next() {
 			var task models.Task
 
-			err = rows.Scan(&task.ID,&task.NAME,&task.STATUS,&task.USERID)
-			if err1!=nil{
-				log.Println("error in scanning the data from the rows",err)
+			err = rows.Scan(&task.ID, &task.NAME, &task.STATUS, &task.USERID)
+			if err1 != nil {
+				log.Println("error in scanning the data from the rows", err)
 			}
 			tasklist = append(tasklist, task)
 		}
-		if err!=nil{
+		if err != nil {
 			log.Println("error in fetching the data")
-			return 
+			return
 		}
 
 		json.NewEncoder(writer).Encode(map[string]interface{}{
-			"message":"the task of the user are",
-			"tasklist":tasklist,
+			"message":  "the task of the user are",
+			"tasklist": tasklist,
 		})
 	}
 }
 
-func UpdateStatusOfTask(db *sql.DB) http.HandlerFunc{
-	return func ( writer http.ResponseWriter, request *http.Request){
+func UpdateStatusOfTask(db *sql.DB) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
 
-		var req struct{
-			TaskId int `json:"taskid"`
-			UserId int `json:"user_id"`
+		var req struct {
+			TaskId int    `json:"taskid"`
+			UserId int    `json:"user_id"`
 			Status string `json:"status"`
 		}
-      err:= json.NewDecoder(request.Body).Decode(&req);
-	  
-	  if err!=nil{
-		log.Println("error in decoding the data")
-		return 
-	  }
-	  
-       res, err := db.Exec(
+		err := json.NewDecoder(request.Body).Decode(&req)
+
+		if err != nil {
+			log.Println("error in decoding the data")
+			return
+		}
+
+		res, err := db.Exec(
 			"UPDATE tasks1 SET status=?, updatedAt=CURRENT_TIMESTAMP WHERE id=? AND userid=? ",
 			req.Status, req.TaskId, req.UserId,
 		)
@@ -174,40 +172,39 @@ func UpdateStatusOfTask(db *sql.DB) http.HandlerFunc{
 
 		writer.Header().Set("Content-type", "application/json")
 		json.NewEncoder(writer).Encode(map[string]interface{}{
-			"message":"status updated succesfully",
-			"result":res,
+			"message": "status updated succesfully",
+			"result":  res,
 		})
 
-	
 	}
 }
 
-func GetTasksBySorted(db *sql.DB) http.HandlerFunc{
+func GetTasksBySorted(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		useridstr:=r.PathValue("userid")
-		if useridstr==""{
+		useridstr := r.PathValue("userid")
+		if useridstr == "" {
 			log.Println("enter a valid user id")
 		}
-		userid,err:=strconv.Atoi(useridstr)
-		if err!=nil{
+		userid, err := strconv.Atoi(useridstr)
+		if err != nil {
 			log.Println("Error")
 		}
 
-		sort:=r.PathValue("sort")
+		sort := r.PathValue("sort")
 
 		query := `SELECT * FROM tasks1
 			WHERE userId = ?`
-		
-		switch sort{
-			case "name":
-				query+="ORDER BY name ASC"
-			case "createdAt":
-				query+="ORDER BY createdAt DESC"
-			case "updatedAt":
-				query+="ORDER BY updatedAt DESC"
-			default:
-				query+="ORDER BY id DESC"
+
+		switch sort {
+		case "name":
+			query += "ORDER BY name ASC"
+		case "createdAt":
+			query += "ORDER BY createdAt DESC"
+		case "updatedAt":
+			query += "ORDER BY updatedAt DESC"
+		default:
+			query += "ORDER BY id DESC"
 
 		}
 		log.Println(query)
@@ -219,76 +216,67 @@ func GetTasksBySorted(db *sql.DB) http.HandlerFunc{
 		tasks := []models.Task{}
 
 		for rows.Next() {
-			rows.Scan(&task.ID, &task.NAME, &task.STATUS, &task.CreatedAt, &task.UpdatedAt,)
+			rows.Scan(&task.ID, &task.NAME, &task.STATUS, &task.USERID, &task.CreatedAt, &task.UpdatedAt)
 			tasks = append(tasks, task)
 		}
 		rows.Close()
 		w.Header().Set("Content-type", "application/json")
 		json.NewEncoder(w).Encode(map[string]interface{}{
-			"tasks":tasks,
+			"tasks": tasks,
 		})
 
 	}
 }
 
-func DeleteTask(db *sql.DB) http.HandlerFunc{
-	return func(writer http.ResponseWriter, request *http.Request){
+func DeleteTask(db *sql.DB) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
 		idstr := request.PathValue("taskid")
 		useridstr := request.PathValue("userid")
 
-
-		if idstr=="" || useridstr==""{
+		if idstr == "" || useridstr == "" {
 			log.Println("userid and taskid  required plz provide ids")
 			return
 		}
 
-		id,err :=strconv.Atoi(idstr)
-		if err!=nil{
-			log.Println("id must be integer",err)
-			return 
+		id, err := strconv.Atoi(idstr)
+		if err != nil {
+			log.Println("id must be integer", err)
+			return
 		}
-        
-		userid,err1:=strconv.Atoi(useridstr);
-		if err1!=nil{
-			log.Println("userid must be integer",err1)
-			return 
+
+		userid, err1 := strconv.Atoi(useridstr)
+		if err1 != nil {
+			log.Println("userid must be integer", err1)
+			return
 		}
-        
+
 		query := `DELETE FROM tasks1 WHERE userid=? AND id=?`
 
-		result,err:=db.Exec(query,userid,id)
+		result, err := db.Exec(query, userid, id)
 
-		if err!=nil{
-			log.Println("error while executing the database query",err)
-			return 
+		if err != nil {
+			log.Println("error while executing the database query", err)
+			return
 		}
 
-		 rowsAffected, err := result.RowsAffected()
+		rowsAffected, err := result.RowsAffected()
 
-        if err!=nil{
-         log.Println("error in checking rows affected", err)
-		 return 
-		}         
- 
-        if rowsAffected == 0 {
-           json.NewEncoder(writer).Encode(map[string]interface{}{
-              "error": "task not found",
-                    })
-            return
-        }
+		if err != nil {
+			log.Println("error in checking rows affected", err)
+			return
+		}
 
-		
+		if rowsAffected == 0 {
+			json.NewEncoder(writer).Encode(map[string]interface{}{
+				"error": "task not found",
+			})
+			return
+		}
 
 		json.NewEncoder(writer).Encode(map[string]interface{}{
-			"message":"task deleted succesfully",
-			"deleted userid":userid,
-			"deleted task":id,
+			"message":        "task deleted succesfully",
+			"deleted userid": userid,
+			"deleted task":   id,
 		})
 	}
 }
-
-
-
-
-
-
