a8ce63ca61520fbfee259c9b0ff2015f2aa5a29d
siddharthkumbhar-code <siddharth.kumbhar@cctech.co.in>
2026-04-09 16:06:38 +0530

    add test in task repo

--- repository/task_repo_test.go
+++ repository/task_repo_test.go
@@ -1,12 +1,24 @@
 package repository
 
-// import (
-// 	"go-sqlite/testutils"
-// 	"testing"
-// )
+import (
+	"go-sqlite/models"
+	"go-sqlite/testutils"
+	"testing"
+)
 
-// func TestInsertTask_Success(t *testing.T){
-// 	db:=testutils.SetupTestDb()
-	
+func TestInsertTask_Success(t *testing.T) {
+	db := testutils.SetupTestDb()
+	defer db.Close()
 
-// }
+	repo := NewTaskRepository(db)
+
+	err := repo.InsertTask(models.Task{
+		Name:   "New Task",
+		Status: "pending",
+		UserId: 1, // valid user exists
+	})
+
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+}
